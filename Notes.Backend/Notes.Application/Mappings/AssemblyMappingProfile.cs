ae916c3cb6daad76c01e57930992a1651a67abd7
dimankan <dimankan94@mail.ru>
2022-06-05 17:24:32 +0300

    Исправлние ошибки в маппинге

---
+++ Notes.Backend/Notes.Application/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Mappings
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) => ApplyMappingFromAssembly(assembly); // это конструктор
+        private void ApplyMappingFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes().Where(type => type.GetInterfaces().Any(i => i.GetGenericTypeDefinition() == typeof(IMapWith<>))).ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] {this});
+            }
+        }
+    }
+}
